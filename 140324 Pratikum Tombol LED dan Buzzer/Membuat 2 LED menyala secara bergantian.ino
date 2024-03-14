5ca1fce243d99dd50f9becf942523d361b0a95f9
anop <81241987+mhmdnvn18@users.noreply.github.com>
2024-03-15 01:52:19 +0700

    Update and rename Membuat 2 LED menyala secara bergantian to Membuat 2 LED menyala secara bergantian.ino

---
+++ 140324 Pratikum Tombol LED dan Buzzer/Membuat 2 LED menyala secara bergantian.ino
@@ -0,0 +1,20 @@
+// Lampu LED Berjalan Menggunakan Wemos D1 R1 UNO
+// Menyalakan LED dengan Aktif HIGH
+
+void setup() {
+  pinMode(D4, OUTPUT);
+  pinMode(D3, OUTPUT);
+
+  // menjadikan PIN 4 dan 3 sebagai OUTPUT
+}
+
+void loop() {
+  // menyala secara bergilir
+  digitalWrite(D4, HIGH);
+  digitalWrite(D3, LOW);
+  delay(1000);
+  
+  digitalWrite(D4, LOW);
+  digitalWrite(D3, HIGH);
+  delay(1000);
+}
