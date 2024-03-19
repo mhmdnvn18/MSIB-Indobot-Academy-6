5c90a1aeb8d33a031e517a60bdb3dc9e03bd4530
anop <81241987+mhmdnvn18@users.noreply.github.com>
2024-03-19 18:51:59 +0700

    Create Praktikum 4 – Memulai Program Arduino.ino

---
+++ 050324 Praktikum Dasar Pemrograman Bahasa C dan Arduino/Praktikum 4 – Memulai Program Arduino.ino
@@ -0,0 +1,11 @@
+void setup() {
+/*Semua program di void setup()
+hanya akan di jalankan sekali saja*/
+Serial.begin(9600);
+Serial.println("Welcome to my world");
+}
+
+void loop() {
+....
+....
+}
