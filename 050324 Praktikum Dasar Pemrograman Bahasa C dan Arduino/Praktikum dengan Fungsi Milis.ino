0253c02e2dc8a94da59605c627fd44b3a495d4c1
anop <81241987+mhmdnvn18@users.noreply.github.com>
2024-03-19 19:09:03 +0700

    Update Praktikum dengan Fungsi Milis.ino

--- 050324 Praktikum Dasar Pemrograman Bahasa C dan Arduino/Praktikum dengan Fungsi Milis.ino
+++ 050324 Praktikum Dasar Pemrograman Bahasa C dan Arduino/Praktikum dengan Fungsi Milis.ino
@@ -1,1 +1,24 @@
+int outputLED = 13;
+long durasiLED = 0;
+long jedaLED = 500;
+int statusLED = LOW;
+void setup()
+{
+  pinMode(outputLED, OUTPUT);
+}
 
+void loop()
+{
+  if (millis()-durasiLED>jedaLED_
+  {durasiLED = millis();
+if (statusLED == LOW)
+{
+statusLED = HIGH;
+}
+else
+}
+statusLED = LOW;
+}
+digitalWrite(13,statusLED);
+}
+}
